8ba7b08b118d93f0905052fb804618a9ac228750
Muslim AbuTaha <muslim.adel@gmail.com>
2016-11-01 23:48:27 +0200

    Small improvements
    - Added version number
    - Renamed 'darwin' to 'mac'

--- cross-compile.sh
+++ cross-compile.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+VERSION="0.2"
+
 rm -rf dist; mkdir -p dist
 for GOOS in darwin linux windows; do
   for GOARCH in 386 amd64; do
     echo "Building $GOOS-$GOARCH"
     if [[ $GOOS == "windows" ]]; then
-       env GOOS=$GOOS GOARCH=$GOARCH go build -o dist/aws-es-proxy-${GOOS}-${GOARCH}.exe
+       env GOOS=$GOOS GOARCH=$GOARCH go build -o dist/aws-es-proxy-${VERSION}-${GOOS}-${GOARCH}.exe
+     elif [[ $GOOS == "darwin" ]]; then
+       env GOOS=$GOOS GOARCH=$GOARCH go build -o dist/aws-es-proxy-${VERSION}-mac-${GOARCH}
     else
-      env GOOS=$GOOS GOARCH=$GOARCH go build -o dist/aws-es-proxy-${GOOS}-${GOARCH}
+      env GOOS=$GOOS GOARCH=$GOARCH go build -o dist/aws-es-proxy-${VERSION}-${GOOS}-${GOARCH}
     fi
   done
 done
