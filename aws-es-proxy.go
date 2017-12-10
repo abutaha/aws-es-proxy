1d5782858399e0846a46e82bcef1c82e8c4f8126
Muslim AbuTaha <muslim.adel@gmail.com>
2017-12-10 18:56:33 +0200

    - Refactor most of the code. - Added ability to stop signing requests. This feature makes aws-es-proxy work with any ElasticSearch installation. - Added ability to log requests and responses to log files.

--- aws-es-proxy.go
+++ aws-es-proxy.go
@@ -16,157 +16,174 @@ import (
 	"strings"
 	"time"
 
+	// log "github.com/Sirupsen/logrus"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/aws/signer/v4"
+	uuid "github.com/satori/go.uuid"
 )
 
+type requestStruct struct {
+	Requestid  string
+	Datetime   string
+	Remoteaddr string
+	Requesturi string
+	Method     string
+	Statuscode int
+	Elapsed    float64
+	Body       string
+}
+
+type responseStruct struct {
+	Requestid string
+	Body      string
+}
+
 type proxy struct {
-	Scheme      string
-	Host        string
-	Region      string
-	Service     string
-	Verbose     bool
-	Prettify    bool
-	Credentials *credentials.Credentials
+	scheme       string
+	host         string
+	region       string
+	service      string
+	endpoint     string
+	verbose      bool
+	prettify     bool
+	logtofile    bool
+	nosignreq    bool
+	fileRequest  *os.File
+	fileResponse *os.File
+	credentials  *credentials.Credentials
 }
 
-func getSigner(p *proxy) *v4.Signer {
-	if p.Credentials == nil {
-		p.Credentials = getCredentials()
-	}
-	return v4.NewSigner(p.Credentials)
-}
-
-func getCredentials() *credentials.Credentials {
-	sess := session.Must(session.NewSession())
-	credentials := sess.Config.Credentials
-	log.Print("Generated fresh AWS Credentials object")
-	return credentials
-}
-
-func copyHeaders(dst, src http.Header) {
-	for k, vals := range src {
-		for _, v := range vals {
-			dst.Add(k, v)
-		}
+func newProxy(args ...interface{}) *proxy {
+	return &proxy{
+		endpoint:  args[0].(string),
+		verbose:   args[1].(bool),
+		prettify:  args[2].(bool),
+		logtofile: args[3].(bool),
+		nosignreq: args[4].(bool),
 	}
 }
 
-func replaceBody(req *http.Request) []byte {
-	if req.Body == nil {
-		return []byte{}
-	}
-	payload, _ := ioutil.ReadAll(req.Body)
-	req.Body = ioutil.NopCloser(bytes.NewReader(payload))
-	return payload
-}
+func (p *proxy) parseEndpoint() error {
+	var link *url.URL
+	var err error
 
-func parseEndpoint(endpoint string, p *proxy) {
-	link, err := url.Parse(endpoint)
-	if err != nil {
-		log.Fatalf("ERROR: Failed parsing endpoint: %s\n", endpoint)
+	if link, err = url.Parse(p.endpoint); err != nil {
+		return fmt.Errorf("error: failure while parsing endpoint: %s. Error: %s",
+			p.endpoint, err.Error())
 	}
 
 	// Only http/https are supported schemes
-	scheme := func(x string) string {
-		switch x {
-		case "http", "https":
-			return x
-		}
-		return "https"
+	switch link.Scheme {
+	case "http", "https":
+	default:
+		link.Scheme = "https"
 	}
-	link.Scheme = scheme(link.Scheme)
 
-	// Unkown schemes sometimes result in empty host value
+	// Unknown schemes sometimes result in empty host value
 	if link.Host == "" {
-		log.Fatalf("ERROR: Empty host information in submitted endpoint (%s)\n", endpoint)
+		return fmt.Errorf("error: empty host or protocol information in submitted endpoint (%s)",
+			p.endpoint)
 	}
 
-	// Extract region and service from link
-	parts := strings.Split(link.Host, ".")
-	var region, service string
+	// AWS SignV4 enabled, extract required parts for signing process
+	if !p.nosignreq {
+		// Extract region and service from link
+		parts := strings.Split(link.Host, ".")
 
-	if len(parts) == 5 {
-		region, service = parts[1], parts[2]
-	} else {
-		log.Fatalln("ERROR: Submitted endpoint is not a valid Amazon ElasticSearch Endpoint")
+		if len(parts) == 5 {
+			p.region, p.service = parts[1], parts[2]
+		} else {
+			return fmt.Errorf("error: submitted endpoint is not a valid Amazon ElasticSearch Endpoint")
+		}
 	}
 
-	// Build proxy struct
-	p.Scheme = link.Scheme
-	p.Host = link.Host
-	p.Region = region
-	p.Service = service
+	// Update proxy struct
+	p.scheme = link.Scheme
+	p.host = link.Host
+
+	return nil
+}
+
+func (p *proxy) getSigner() *v4.Signer {
+	// Refresh credentials after expiration. Required for STS
+	if p.credentials == nil {
+		sess := session.Must(session.NewSession())
+		credentials := sess.Config.Credentials
+		p.credentials = credentials
+		log.Println("Generated fresh AWS Credentials object")
+	}
+	return v4.NewSigner(p.credentials)
 }
 
 func (p *proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	requestStarted := time.Now()
 	dump, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		log.Fatalln("error while dumping request. Error: ", err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 	defer r.Body.Close()
 
-	respondError := func(err error) {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(err.Error()))
-	}
+	ep := *r.URL
+	ep.Host = p.host
+	ep.Scheme = p.scheme
 
-	endpoint := *r.URL
-	endpoint.Host = p.Host
-	endpoint.Scheme = p.Scheme
-
-	req, err := http.NewRequest(r.Method, endpoint.String(), r.Body)
+	req, err := http.NewRequest(r.Method, ep.String(), r.Body)
 	if err != nil {
-		respondError(err)
+		log.Fatalln("error creating new request. ", err.Error())
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	// Workaround for ES 5.1 and Kibana 5.1.1
-	if val, ok := r.Header["Kbn-Version"]; ok {
-		req.Header.Set("Kbn-Version", val[0])
+	addHeaders(r.Header, req.Header)
+
+	// Make signV4 optional
+	if !p.nosignreq {
+		// Start AWS session from ENV, Shared Creds or EC2Role
+		signer := p.getSigner()
+
+		// Sign the request with AWSv4
+		payload := bytes.NewReader(replaceBody(req))
+		signer.Sign(req, payload, p.service, p.region, time.Now())
 	}
-
-	// Workaround for ES 6.0
-	if val, ok := r.Header["Content-Type"]; ok {
-		if val[0] == "application/json" {
-			req.Header.Set("Content-Type", "application/json")
-		}
-	}
-
-	// Start AWS session from ENV, Shared Creds or EC2Role
-	signer := getSigner(p)
-
-	// Sign the request with AWSv4
-	payload := bytes.NewReader(replaceBody(req))
-	signer.Sign(req, payload, p.Service, p.Region, time.Now())
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		log.Println(err)
-		respondError(err)
+		log.Fatalln(err.Error())
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	if resp.StatusCode == 403 {
-		p.Credentials = nil
-		return
+	if !p.nosignreq {
+		// AWS credentials expired, need to generate fresh ones
+		if resp.StatusCode == 403 {
+			p.credentials = nil
+			return
+		}
 	}
 
 	defer resp.Body.Close()
 
-	// Write back received headers
+	// Write back headers to requesting client
 	copyHeaders(w.Header(), resp.Header)
 
-	buf := bytes.Buffer{}
-	if _, err := io.Copy(&buf, resp.Body); err != nil {
-		log.Fatal(err)
+	// Send response back to requesting client
+	body := bytes.Buffer{}
+	if _, err := io.Copy(&body, resp.Body); err != nil {
+		log.Fatalln(err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
-
-	// Send response back
 	w.WriteHeader(resp.StatusCode)
-	w.Write(buf.Bytes())
+	w.Write(body.Bytes())
 
-	// Log everything
-	remoteAddr := r.RemoteAddr
+	requestEnded := time.Since(requestStarted)
+
+	/*############################
+	## Logging
+	############################*/
+
 	rawQuery := string(dump)
 	rawQuery = strings.Replace(rawQuery, "\n", " ", -1)
 	regex, _ := regexp.Compile("{.*}")
@@ -181,10 +198,8 @@ func (p *proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		query = ""
 	}
 
-	if p.Verbose {
-		requestEnded := time.Since(requestStarted)
-
-		if p.Prettify {
+	if p.verbose {
+		if p.prettify {
 			var prettyBody bytes.Buffer
 			json.Indent(&prettyBody, []byte(query), "", "  ")
 			t := time.Now()
@@ -192,33 +207,105 @@ func (p *proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			fmt.Println()
 			fmt.Println("========================")
 			fmt.Println(t.Format("2006/01/02 15:04:05"))
-			fmt.Println("Remote Address: ", remoteAddr)
-			fmt.Println("Request URI: ", endpoint.RequestURI())
+			fmt.Println("Remote Address: ", r.RemoteAddr)
+			fmt.Println("Request URI: ", ep.RequestURI())
 			fmt.Println("Method: ", r.Method)
 			fmt.Println("Status: ", resp.StatusCode)
 			fmt.Printf("Took: %.3fs\n", requestEnded.Seconds())
 			fmt.Println("Body: ")
 			fmt.Println(string(prettyBody.Bytes()))
-			fmt.Println("========================")
-
 		} else {
 			log.Printf(" -> %s; %s; %s; %s; %d; %.3fs\n",
-				r.Method, remoteAddr, endpoint.RequestURI(), query, resp.StatusCode, requestEnded.Seconds())
+				r.Method, r.RemoteAddr,
+				ep.RequestURI(), query,
+				resp.StatusCode, requestEnded.Seconds())
+		}
+	}
+
+	if p.logtofile {
+		requestID := fmt.Sprintf("%s", uuid.NewV4().String())
+
+		reqStruct := &requestStruct{
+			Requestid:  requestID,
+			Datetime:   time.Now().Format("2006/01/02 15:04:05"),
+			Remoteaddr: r.RemoteAddr,
+			Requesturi: ep.RequestURI(),
+			Method:     r.Method,
+			Statuscode: resp.StatusCode,
+			Elapsed:    requestEnded.Seconds(),
+			Body:       query,
+		}
+
+		respStruct := &responseStruct{
+			Requestid: requestID,
+			Body:      string(body.Bytes()),
+		}
+
+		y, _ := json.Marshal(reqStruct)
+		z, _ := json.Marshal(respStruct)
+		p.fileRequest.Write(y)
+		p.fileRequest.WriteString("\n")
+		p.fileResponse.Write(z)
+		p.fileResponse.WriteString("\n")
+
+	}
+
+}
+
+// Recent versions of ES/Kibana require
+// "kbn-version" and "content-type: application/json"
+// headers to exist in the request.
+// If missing requests fails.
+func addHeaders(src, dest http.Header) {
+	if val, ok := src["Kbn-Version"]; ok {
+		dest.Add("Kbn-Version", val[0])
+	}
+
+	if val, ok := src["Content-Type"]; ok {
+		if val[0] == "application/json" {
+			dest.Add("Content-Type", "application/json")
+		}
+	}
+}
+
+// Signer.Sign requires a "seekable" body to sum body's sha256
+func replaceBody(req *http.Request) []byte {
+	if req.Body == nil {
+		return []byte{}
+	}
+	payload, _ := ioutil.ReadAll(req.Body)
+	req.Body = ioutil.NopCloser(bytes.NewReader(payload))
+	return payload
+}
+
+func copyHeaders(dst, src http.Header) {
+	for k, vals := range src {
+		for _, v := range vals {
+			dst.Add(k, v)
 		}
 	}
 }
 
 func main() {
-	var endpoint, listenAddress string
-	var verbose bool
-	var prettify bool
 
-	// TODO: Use a more sophisticated args parser that can enforce arguments
+	var (
+		verbose       bool
+		prettify      bool
+		logtofile     bool
+		nosignreq     bool
+		endpoint      string
+		listenAddress string
+		fileRequest   *os.File
+		fileResponse  *os.File
+		err           error
+	)
+
 	flag.StringVar(&endpoint, "endpoint", "", "Amazon ElasticSearch Endpoint (e.g: https://dummy-host.eu-west-1.es.amazonaws.com)")
 	flag.StringVar(&listenAddress, "listen", "127.0.0.1:9200", "Local TCP port to listen on")
 	flag.BoolVar(&verbose, "verbose", false, "Print user requests")
-	flag.BoolVar(&prettify, "pretty", false, "Prettify verbose output")
-
+	flag.BoolVar(&logtofile, "log-to-file", false, "Log user requests and ElasticSearch responses to files")
+	flag.BoolVar(&prettify, "pretty", false, "Prettify verbose and file output")
+	flag.BoolVar(&nosignreq, "no-sign-reqs", false, "Disable AWS Signature v4")
 	flag.Parse()
 
 	if len(os.Args) < 3 {
@@ -227,9 +314,38 @@ func main() {
 		os.Exit(1)
 	}
 
-	mux := &proxy{Verbose: verbose, Prettify: prettify}
-	parseEndpoint(endpoint, mux)
+	p := newProxy(
+		endpoint,
+		verbose,
+		prettify,
+		logtofile,
+		nosignreq,
+	)
 
-	fmt.Printf("Listening on %s\n", listenAddress)
-	log.Fatal(http.ListenAndServe(listenAddress, mux))
+	if err = p.parseEndpoint(); err != nil {
+		log.Fatalln(err)
+		os.Exit(1)
+	}
+
+	if p.logtofile {
+		requestFname := fmt.Sprintf("request-%s.log", uuid.NewV4().String())
+		responseFname := fmt.Sprintf("response-%s.log", uuid.NewV4().String())
+
+		if fileRequest, err = os.Create(requestFname); err != nil {
+			log.Fatalln(err.Error())
+		}
+		if fileResponse, err = os.Create(responseFname); err != nil {
+			log.Fatalln(err.Error())
+		}
+
+		defer fileRequest.Close()
+		defer fileResponse.Close()
+
+		p.fileRequest = fileRequest
+		p.fileResponse = fileResponse
+
+	}
+
+	log.Printf("Listening on %s...\n", listenAddress)
+	log.Fatal(http.ListenAndServe(listenAddress, p))
 }
