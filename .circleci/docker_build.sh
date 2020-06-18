31bd9e6a6f63ddf95bfdebc709ad83457b32718a
Thiago Francisco <nullck@gmail.com>
2020-06-18 14:59:12 -0300

    test tags

--- .circleci/docker_build.sh
+++ .circleci/docker_build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 WORKDIR=$(echo $PWD | sed 's/\/.circleci//')
-IMAGE_TAG=`echo ${CIRCLE_SHA1} | grep -o -E '.{0,8}'| head -n1`
-docker build -t ${ECR_CENTRAL_REPOSITORY}/${ECR_REPO_NAME}:${IMAGE_TAG} .
+docker build -t ${ECR_CENTRAL_REPOSITORY}/${ECR_REPO_NAME} .
 mkdir -p ${WORKDIR}/docker-cache
-docker save -o ${WORKDIR}/docker-cache/built-image.tar ${ECR_CENTRAL_REPOSITORY}/${ECR_REPO_NAME}:${IMAGE_TAG}
+docker save -o ${WORKDIR}/docker-cache/built-image.tar ${ECR_CENTRAL_REPOSITORY}/${ECR_REPO_NAME}
 
--- .circleci/docker_push.sh
+++ .circleci/docker_push.sh
@@ -5,5 +5,6 @@ pip install --user awscli
 export PATH="$(python -m site --user-base)/bin:${PATH}"
 $(aws ecr get-login --registry-ids 387984977604 --no-include-email --region eu-west-1)
 docker load < ${WORKDIR}/docker-cache/built-image.tar
-IMAGE_TAG=`docker image ls | grep "${ECR_CENTRAL_REPOSITORY}/${ECR_REPO_NAME}" | awk '{print $2}'`
-docker push ${ECR_CENTRAL_REPOSITORY}/${ECR_REPO_NAME}:${IMAGE_TAG}
+[[ ! -z ${CIRCLE_TAG} ]] || { echo "No git tag in this pipeline exiting ...."; exit 0; }
+docker tag ${ECR_CENTRAL_REPOSITORY}/${ECR_REPO_NAME} ${ECR_CENTRAL_REPOSITORY}/${ECR_REPO_NAME}:${CIRCLE_TAG}
+docker push ${ECR_CENTRAL_REPOSITORY}/${ECR_REPO_NAME}:${CIRCLE_TAG}
